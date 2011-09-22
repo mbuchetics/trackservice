fab1a7fc38857f376120770f896c36641fb28f20
Matthias Buchetics <mbuchetics@gmail.com>
2011-09-22 11:55:38 +0200

    added Backbone and some simple routes, updated to Bootstrap 1.3.0

--- public/js/app.js
+++ public/js/app.js
@@ -3,23 +3,43 @@
 // main app
 // ========
 
-$(document).ready(function () {
+$(function() {
 	var host = window.location.hostname,
 		songListTempl = Handlebars.compile($('#song_list_template').html());
-
-	function updateRecentSongsList() {	
-		$.getJSON('api/last/10', function(data) {
-			//console.log(data);
-			
-			var songListHtml = songListTempl({ 
-				songs: data, 
-			});
-			
-			$('#song_list').html(songListHtml);
-			$("time.timeago").timeago();
-		});	
+	
+	function renderSongsList(songs) {
+	    var songListHtml = songListTempl({ 
+			songs: songs, 
+		});
+		
+		$('#song_list').html(songListHtml);
+		$("time.timeago").timeago();
 	}
 	
-	updateRecentSongsList();
-
+	var AppRouter = Backbone.Router.extend({
+	    routes: {
+	        "/all": "showAll",
+	        "/recent": "showRecent",
+	        "": "showRecent",
+	    },
+	    
+	    showAll: function() {
+	        console.log('showAll');
+	        $('#page_title').html('All Songs');
+	        $.getJSON('api/all', function(songs) {
+	            renderSongsList(songs);
+        	});
+	    },
+	    
+	    showRecent: function() {
+	        console.log('showRecent');
+	        $('#page_title').html('Last 10 Songs');
+	        $.getJSON('api/last/10', function(songs) {
+	            renderSongsList(songs);
+        	});
+	    }
+	});
+	
+	var appRouter = new AppRouter();
+	Backbone.history.start();
 });
