ea1f93d76eb78f20cd505f07e4640d1f3f10d1b1
Matthias Buchetics <mbuchetics@gmail.com>
2011-10-15 19:15:23 +0200

    moved server files

--- config.js
+++
@@ -1,6 +0,0 @@
-var config = {};
-
-config.port = process.env.PORT || 3000;
-config.db_url = process.env.MONGODB_URL || 'mongodb://localhost:27017/music';
-
-module.exports = config;
--- environment.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(app, express) {
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    //app.use(express.staticCache());
-    app.use(express.static(__dirname + '/public'));
-};
