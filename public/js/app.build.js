5801557db6ca508d0c95bb8f6b68c08da70e211d
Matthias Buchetics <mbuchetics@gmail.com>
2011-11-24 17:13:49 +0100

    added r.js optimization configuration

---
+++ public/js/app.build.js
@@ -0,0 +1,18 @@
+({
+    appDir: "../",
+    baseUrl: "js",
+    dir: "../../public-build",
+    paths: {
+        "libs.json2":       "libs/require_json2",
+        "libs.jquery":      "libs/require_jquery",
+        "libs.underscore":  "libs/require_underscore",
+        "libs.handlebars":  "libs/require_handlebars",
+        "libs.backbone":    "libs/require_backbone",
+        "libs.date":        "libs/require_date"
+    },
+    modules: [
+        {
+            name: "app"
+        }
+    ]
+})
