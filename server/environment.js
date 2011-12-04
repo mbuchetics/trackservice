b0df486351a3ff930639eddb269644167b753373
Matthias Buchetics <mbuchetics@gmail.com>
2011-12-04 12:17:09 +0100

    and removing staticCache again ...

--- server/environment.js
+++ server/environment.js
@@ -4,6 +4,6 @@ module.exports = function(app, express) {
     
     app.use(express.bodyParser());
     app.use(express.methodOverride());
-    app.use(express.staticCache());
+    //app.use(express.staticCache());
     app.use(express.static(__dirname + '/../public', { maxAge: oneDay }));
 };
