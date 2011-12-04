de7ee40c2070ac142bc94a3b3276efec48bdd31f
Matthias Buchetics <mbuchetics@gmail.com>
2011-12-04 11:09:30 +0100

    added static caching of expressjs

--- server/environment.js
+++ server/environment.js
@@ -1,6 +1,6 @@
 module.exports = function(app, express) {
     app.use(express.bodyParser());
     app.use(express.methodOverride());
-    //app.use(express.staticCache());
+    app.use(express.staticCache());
     app.use(express.static(__dirname + '/../public'));
 };
