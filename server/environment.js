2899e65777174d749c4e812bd6581a823b5b3736
Matthias Buchetics <mbuchetics@gmail.com>
2011-12-04 11:26:26 +0100

    trying out maxAge for static file serving

--- server/environment.js
+++ server/environment.js
@@ -1,6 +1,9 @@
 module.exports = function(app, express) {
+    
+    var oneDay = 86400000; // milliseconds
+    
     app.use(express.bodyParser());
     app.use(express.methodOverride());
     app.use(express.staticCache());
-    app.use(express.static(__dirname + '/../public'));
+    app.use(express.static(__dirname + '/../public', { maxAge: oneDay }));
 };
