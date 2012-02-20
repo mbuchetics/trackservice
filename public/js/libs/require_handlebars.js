e6aa529d1b7f2380c71c10b7d8a5e1a30e344316
Matthias Buchetics <mbuchetics@gmail.com>
2012-02-20 09:43:29 +0100

    optimized public

--- public/js/libs/require_handlebars.js
+++ public/js/libs/require_handlebars.js
@@ -1,4 +1,4 @@
-define(["js/libs/externals/handlebars.1.0.0.beta.6.js"], 
+define(["js/libs/externals/handlebars-1.0.0.beta.6.js"], 
     function() {
         if (window.console) 
             console.log('handlebars');
